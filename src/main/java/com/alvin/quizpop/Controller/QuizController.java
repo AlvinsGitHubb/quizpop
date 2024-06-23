c94e1bc9e071eb3c59dc58fbd173e61544b35b13
AlvinsGitHubb <alvinmathew0627@gmail.com>
2024-06-22 20:28:56 -0500

    fixed question population.

--- src/main/java/com/alvin/quizpop/Controller/QuizController.java
+++ src/main/java/com/alvin/quizpop/Controller/QuizController.java
@@ -23,17 +23,47 @@ public class QuizController {
     @Autowired
     private QuizService quizService;
 
+    /*
+     * ex: http://localhost:8080/quiz/create?category=Java&numQ=5&title=JQuiz
+     * creates a quiz under category - java, 5 questions, and title of JQuiz
+     * can be found in pgadmin
+     */
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ,
             @RequestParam String title) {
         return quizService.createQuiz(category, numQ, title);
     }
 
+    // ex: http://localhost:8080/quiz/get/32
     @GetMapping("get/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id) {
         return quizService.getQuizQuestions(id);
     }
 
+    /*
+     * go into postman body (raw, JSON format)
+     * [
+     * {
+     * "id":18
+     * "response":3
+     * }
+     * {
+     * "id":2
+     * "response":4
+     * }
+     * {
+     * "id":5
+     * "response":1
+     * }
+     * {
+     * "id":6
+     * "response":2
+     * }
+     * ]
+     * so put in some body like this into post man and have the link be
+     * /quiz/submit/32 or something and
+     * it will print a number representing your score
+     */
     @PostMapping("submit/{id}")
     public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses) {
         return quizService.calculateResult(id, responses);
