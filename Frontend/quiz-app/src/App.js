dd30abe202e599fa912882bb19d49ea3599540f0
AlvinsGitHubb <alvinmathew0627@gmail.com>
2024-09-16 21:33:40 -0500

    Revert "Update Frontend"
    This reverts commit 63f1cf7dd2d871c539dd49d9545c3ce1bd304454.

--- Frontend/quiz-app/src/App.js
+++ Frontend/quiz-app/src/App.js
@@ -18,9 +18,9 @@ function App() {
             <Route path="/" element={<QuestionList />} />
             <Route path="/add" element={<CreateQuestion />} />
             <Route path="/edit/:id" element={<UpdateQuestion />} />
-            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/quiz/:id" element={<Quiz />} />
           </Routes>
-          <Link to="/quiz" className="btn btn-primary">Start Quiz</Link>
+          <Link to="/quiz/1" className="btn btn-primary">Start Quiz</Link> {/* Adjust quiz ID as needed */}
         </div>
       </div>
     </Router>
