a8f2e1e7a10bb0979599a497b7041cdaf8ec83cb
AlvinsGitHubb <alvinmathew0627@gmail.com>
2024-09-19 17:50:46 -0500

    Rename Components folder to fix case sensitivity

--- Frontend/quiz-app/src/App.js
+++ Frontend/quiz-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import QuestionList from './components/QuestionList';
-import CreateQuestion from './components/CreateQuestion';
-import UpdateQuestion from './components/UpdateQuestion';
-import Quiz from './components/Quiz';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import QuestionList from './Components/QuestionList';
+import CreateQuestion from './Components/CreateQuestion';
+import UpdateQuestion from './Components/UpdateQuestion';
+import Quiz from './Components/Quiz';
 import './App.css';
 
 function App() {
+  const location = useLocation(); // Get the current location (URL)
+
   return (
     <Router>
       <div className="App">
@@ -20,8 +22,12 @@ function App() {
             <Route path="/edit/:id" element={<UpdateQuestion />} />
             <Route path="/quiz" element={<Quiz />} />
           </Routes>
-          <Link to="/quiz" className="btn btn-primary">Start Quiz</Link>
         </div>
+        
+        {/* Only show the Start Quiz button on the home page ("/") */}
+        {location.pathname === "/" && (
+          <Link to="/quiz" className="btn btn-primary">Start Quiz</Link>
+        )}
       </div>
     </Router>
   );
