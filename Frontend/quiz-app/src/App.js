1264fa8619dc100a59c4228b30e2d678cb92607a
AlvinsGitHubb <alvinmathew0627@gmail.com>
2024-09-19 18:07:26 -0500

    Fix Start Quiz Button

--- Frontend/quiz-app/src/App.js
+++ Frontend/quiz-app/src/App.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
-import QuestionList from './Components/QuestionList';
-import CreateQuestion from './Components/CreateQuestion';
-import UpdateQuestion from './Components/UpdateQuestion';
-import Quiz from './Components/Quiz';
+import { Route, Routes, Link, useLocation } from 'react-router-dom';  // No need to import BrowserRouter here
+import QuestionList from './components/QuestionList';
+import CreateQuestion from './components/CreateQuestion';
+import UpdateQuestion from './components/UpdateQuestion';
+import Quiz from './components/Quiz';
 import './App.css';
 
 function App() {
-  const location = useLocation(); // Get the current location (URL)
+  const location = useLocation();  // Now we can use useLocation here because the App is wrapped in Router in index.js
 
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h2>Quiz Application</h2>
-        </header>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<QuestionList />} />
-            <Route path="/add" element={<CreateQuestion />} />
-            <Route path="/edit/:id" element={<UpdateQuestion />} />
-            <Route path="/quiz" element={<Quiz />} />
-          </Routes>
-        </div>
-        
-        {/* Only show the Start Quiz button on the home page ("/") */}
-        {location.pathname === "/" && (
-          <Link to="/quiz" className="btn btn-primary">Start Quiz</Link>
-        )}
+    <div className="App">
+      <header className="App-header">
+        <h2>Quiz Application</h2>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<QuestionList />} />
+          <Route path="/add" element={<CreateQuestion />} />
+          <Route path="/edit/:id" element={<UpdateQuestion />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
       </div>
-    </Router>
+      
+      {/* Only show the Start Quiz button on the home page ("/") */}
+      {location.pathname === "/" && (
+        <Link to="/quiz" className="btn btn-primary">Start Quiz</Link>
+      )}
+    </div>
   );
 }
 
